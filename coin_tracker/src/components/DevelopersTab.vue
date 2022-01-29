8440690db8c364ee1bd0e58994461f481e2e9156
burakhanaksoy <burakhan.aksoy@tedu.edu.tr>
2022-01-29 14:30:37 +0300

    add developers tab

---
+++ coin_tracker/src/components/DevelopersTab.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container">
+    <a-card hoverable style="width: 300px">
+      <img
+        slot="cover"
+        alt="example"
+        :src="require('../assets/' + `burak-photo.png`)"
+      />
+      <template slot="actions" class="ant-card-actions">
+        <a href="mailto:burakhanaksoy@gmail.com">
+          <a-icon key="mail" type="mail" />
+        </a>
+        <a href="https://www.linkedin.com/in/burakhan-aksoy/" target="_blank">
+          <img
+            :src="require('../assets/' + `linkedin-img.png`)"
+            style="width: 15px; height: 15px"
+          />
+        </a>
+      </template>
+      <a-card-meta
+        title="Burakhan Aksoy"
+        description="Junior Fullstack Developer"
+      >
+      </a-card-meta>
+    </a-card>
+  </div>
+</template>
+<script>
+export default {
+  components: {},
+  mounted() {},
+  data() {
+    return {};
+  },
+  methods: {
+    sendEmail() {
+      console.log("sends email");
+    },
+  },
+  watch: {},
+};
+</script>
+
+<style scoped>
+.container {
+  width: 50%;
+  display: flex;
+  justify-content: space-between;
+}
+</style>
--- coin_tracker/src/components/Tabs/Tabs.vue
+++ coin_tracker/src/components/Tabs/Tabs.vue
@@ -7,16 +7,21 @@
       <a-tab-pane key="2" tab="Top Coins">
         <coin-table />
       </a-tab-pane>
+      <a-tab-pane key="3" tab="Developers">
+        <developers-tab />
+      </a-tab-pane>
     </a-tabs>
   </div>
 </template>
 <script>
 import CoinTrackChart from "@/components/CoinTrackChart.vue";
 import CoinTable from "@/components/CoinTable.vue";
+import DevelopersTab from "@/components/DevelopersTab.vue";
 export default {
   components: {
     CoinTrackChart,
     CoinTable,
+    DevelopersTab,
   },
   data() {
     return {};
